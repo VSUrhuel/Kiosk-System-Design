f81a24a1e1f9f67bae93ac1acdf5a59459f97347
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2024-02-26 22:19:29 +0800

    Add project files.

---
+++ Kiosk System Design/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk_System_Design
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
